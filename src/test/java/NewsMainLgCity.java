2aaade26fa87e22a5830dfdcc2887f800da1b545
pavdem2012 <pavdem2012@gmail.com>
2021-10-15 18:46:24 +0700

    add SearchByName pozitive Test

--- src/test/java/NewsMainLgCity.java
+++ src/test/java/NewsMainLgCity.java
@@ -1,7 +1,11 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class NewsMainLgCity extends Settings{
     /*
@@ -13,7 +17,52 @@ public class NewsMainLgCity extends Settings{
         Actions scroll = new Actions(driver);
         scroll.moveToElement(driver.findElement(By.className("news"))).perform();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("news__item")));
-        driver.findElement(By.id("bx_651765591_769484")).click();
+        int newsBlock =driver.findElements(By.xpath("//div[@class = 'news__item']")).size();
+        System.out.println(newsBlock);
+        Random random =new Random();
+        int randomNumber = random.nextInt(newsBlock)+1;
+        driver.findElement(By.xpath("(//div[@class = 'news__item']//a[not(contains(text(), 'Бестселлеры коллекций'))])["+randomNumber+"]")).click();
+        //driver.findElement(By.id("bx_651765591_783616")).click();
         wait.until(ExpectedConditions.textToBe(By.className("news-page__back-title"),"Акции и новости"));
     }
+/*    *//*
+    Проверка "Шапка сайта"
+     *//*
+    *//*+
+    Проверка иконки поиска
+     *//*
+    @Test
+    public void SiteHeader() throws InterruptedException {
+        open("https://lgcity.ru");
+        driver.findElement(By.xpath("//a[@class='header__r-icons-link header__r-icons-link--search js-popup']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[contains(text(),'Что вы хотите найти?')]")));
+        driver.findElement(By.className("header__r-icon--close")).click();
+        Thread.sleep(3000);
+    }
+        *//*
+        Проверка иконок Корзины и избранного
+         *//*
+    @Test
+    public void favBasket(){
+        open("https://lgcity.ru");
+        driver.findElement(By.xpath("//a[@class='header__r-icons-link header__r-icons-link--favorite js-header-favorite']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("lk__title")));
+        driver.findElement(By.xpath("//a[contains(text(),'Перейти на главную')]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("header__logo-link")));
+        driver.findElement(By.xpath("//a[@class='header__r-icons-link js-popup js-header-basket']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("lgcity-basket")));
+        driver.findElement(By.xpath("//div[@class='basket__header-icon-box js-popup-close']")).click();
+        driver.findElement(By.className("header__logo-link")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("header__logo-link")));
+        driver.findElement(By.xpath("//a[@class='header__logo-link']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Личный кабинет')]")));
+        driver.findElement(By.xpath("//div[@class='login__title-icon-box js-popup-close']")).click();
+    }
+    @Test
+    public void fav() throws InterruptedException {
+
+        open("https://lgcity.ru");
+        driver.findElement(By.xpath("//a[@class='header__r-icons-link js-popup js-header-basket']")).click();
+        Thread.sleep(3000);
+    }*/
 }
--- src/test/java/TestMainMenu.java
+++ src/test/java/TestMainMenu.java
@@ -2,7 +2,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TestMainMenu extends Settings {
@@ -28,23 +30,74 @@ public class TestMainMenu extends Settings {
     Проверка вложеных элементов главного меню
      */
     @Test
-    public void testCategory() {
+    public void testCategory() throws InterruptedException {
         open("https://lgcity.ru");
-        int menuItems = driver.findElements(By.xpath("//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list']/div"
-                + "/a")).size() - 4;
-        Random random = new Random();
-        int randomItem = random.nextInt(menuItems) + 1;
-        Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list"
-                + "']/div/a)[" + randomItem + "]"))).perform();
-        int menuItemsInner =
-                driver.findElements(By.xpath("//div[@id='gmenu-tab-327']//div[@class='header__nav-list" + "-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category-col" + "']//a")).size();
-        int randomInner = random.nextInt(menuItemsInner) + 1;
-        String randomItemInner =
-                driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class='header__nav" + "-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category" + "-col']//a)[" + randomInner + "]")).getText().toLowerCase();
-        driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class='header__nav-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category-col']//a)[" + randomInner + "]")).click();
-        String randomPageHeader = driver.findElement(By.tagName("h1")).getText().toLowerCase();
-        Assert.assertTrue(randomPageHeader.contains(randomItemInner));
+        driver.findElement(By.id("confirm-use-cookies")).click();
+        ArrayList<String> cartItemInfo = new ArrayList<>();
+        ArrayList<String> inCartItemInfo = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            int menuItems = driver.findElements(By.xpath("//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list']/div" + "/a")).size() - 4;
+            Random random = new Random();
+            int randomItem = random.nextInt(menuItems) + 1;
+            Actions actions = new Actions(driver);
+            actions.moveToElement(driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list" + "']/div/a)[" + randomItem + "]"))).perform();
+            int menuItemsInner = driver.findElements(By.xpath("//div[@id='gmenu-tab-327']//div[@class='header__nav-list" + "-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category-col" + "']//a")).size();
+            int randomInner = random.nextInt(menuItemsInner) + 1;
+            String randomItemInner = driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class='header__nav" + "-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category" + "-col']//a)[" + randomInner + "]")).getText().toLowerCase();
+            driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class='header__nav-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category-col']//a)[" + randomInner + "]")).click();
+            String randomPageHeader = driver.findElement(By.tagName("h1")).getText().toLowerCase();
+            Assert.assertTrue(randomPageHeader.contains(randomItemInner));
+
+            int catalogItems = driver.findElements(By.xpath("//a[@class='catalog__item catalog__item--new " + "owox-catalog-list']")).size();
+            System.out.println("Длина меню: " + catalogItems);
+            int randomCatalogItem = random.nextInt(catalogItems) + 1;
+            driver.findElement(By.xpath("(//a[@class='catalog__item catalog__item--new owox-catalog-list'])[" + randomCatalogItem + "]")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card']")));
+            cartItemInfo.add(driver.findElement(By.xpath("//div[@class='card__info-title']")).getText().toLowerCase());
+            String nameOfProduct = driver.findElement(By.xpath("//div[@class='card__info-title']")).getText().toLowerCase();
+            driver.findElement(By.xpath("//input[@class='js-select-input select__input']")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='select__drop select__drop--sizes']")));
+            cartItemInfo.add(driver.findElement(By.xpath("//ul[@id='offer-sizes']/li[contains(@class, 'selected')]")).getAttribute("data-size").toLowerCase());
+            String sizeOfProduct = driver.findElement(By.xpath("//ul[@id='offer-sizes']/li[contains(@class, 'selected')]")).getAttribute("data-size").toLowerCase();
+            cartItemInfo.add(driver.findElement(By.xpath("//div[@id='offer-price']/div")).getText().replaceAll(" ", ""));
+            String priceOfProduct = driver.findElement(By.xpath("//div[@id='offer-price']/div")).getText().replaceAll(" ", "");
+            driver.findElement(By.xpath("//div[@id='offer-price']/div")).click();
+            System.out.println("цена: " + priceOfProduct);
+            System.out.println("название: " + nameOfProduct);
+            System.out.println("размер: " + sizeOfProduct);
+            driver.findElement(By.id("btn-add-to-cart")).click();
+            driver.findElement(By.xpath("//a[@class='header__logo-link']")).click();
+        }
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='basket-counter__value' and contains (text(),'2')]")));
+        driver.findElement(By.xpath("//a[@class='header__r-icons-link js-popup js-header-basket']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='basket__non-empty']")));
+        for (int i = 0; i < 2; i++) {
+
+
+            inCartItemInfo.add(driver.findElement(By.xpath("//div[@class='basket__item-price']/div")).getText().replaceAll(" ", ""));
+            String priceOfProductInCart = driver.findElement(By.xpath("//div[@class='basket__item-price']/div")).getText().replaceAll(" ", "");
+            inCartItemInfo.add(driver.findElement(By.xpath("//div[@class='basket__item-title']")).getText().toLowerCase());
+            String nameOfProductInCart = driver.findElement(By.xpath("//div[@class='basket__item-title']")).getText().toLowerCase();
+            inCartItemInfo.add(driver.findElement(By.xpath("//div[@class='basket__item-prop']")).getText().replaceAll(" ", "").toLowerCase());
+            String sizeOfProductInCart = driver.findElement(By.xpath("//div[@class='basket__item-prop']")).getText().replaceAll(" ", "").toLowerCase();
+            System.out.println("цена в корзине: " + priceOfProductInCart);
+            System.out.println("имя в корзине: " + nameOfProductInCart);
+            System.out.println("размер в корзине: " + sizeOfProductInCart);
+        }
+        System.out.println("список: " + cartItemInfo);
+        System.out.println("список в корзине: " + inCartItemInfo);
+        /*Assert.assertTrue(priceOfProduct.contains(priceOfProductInCart));
+        Assert.assertTrue(nameOfProduct.contains(nameOfProductInCart));
+        Assert.assertTrue(sizeOfProductInCart.contains(sizeOfProduct));*/
+        for (int i = 0; i < 2; i++) {
+            driver.findElement(By.xpath("//div[@class='basket__item-remove js-basket-remove-item']")).click();
+        }
+        driver.findElement(By.xpath("//div[@class='basket__header-icon-box js-popup-close']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'basket__empty-title')]")));
+        Thread.sleep(3000);
+
+
     }
 }
 
