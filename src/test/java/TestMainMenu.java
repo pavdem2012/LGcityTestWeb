8f8293ae2e7655694a89d1b019e467da3c401e3f
pavdem2012 <pavdem2012@gmail.com>
2021-10-20 19:09:11 +0700

    add SearchByName pozitive Test

--- src/test/java/TestMainMenu.java
+++ src/test/java/TestMainMenu.java
@@ -1,10 +1,12 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class TestMainMenu extends Settings {
@@ -19,8 +21,8 @@ public class TestMainMenu extends Settings {
         Random random = new Random();
         int randomItem = random.nextInt(menuItems) + 1;
         String randomMenuItem =
-                driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav" + "-list']/div/a)" +
-                        "[" + randomItem + "]")).getText().toLowerCase();
+                driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav" + "-list']/div/a)"
+                        + "[" + randomItem + "]")).getText().toLowerCase();
         driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list']/div/a)[" + randomItem + "]")).click();
         String randomPageHeader = driver.findElement(By.tagName("h1")).getText().toLowerCase();
         Assert.assertTrue(randomPageHeader.contains(randomMenuItem));
@@ -33,70 +35,97 @@ public class TestMainMenu extends Settings {
     public void testCategory() throws InterruptedException {
         open("https://lgcity.ru");
         driver.findElement(By.id("confirm-use-cookies")).click();
-        ArrayList<String> cartItemInfo = new ArrayList<>();
-        ArrayList<String> inCartItemInfo = new ArrayList<>();
+        ArrayList<String> cartItemPrice = new ArrayList<>();
+        ArrayList<String> cartItemSize = new ArrayList<>();
+        ArrayList<String> cartItemName = new ArrayList<>();
+        ArrayList<String> cartItemColor = new ArrayList<>();
         for (int i = 0; i < 2; i++) {
-            int menuItems = driver.findElements(By.xpath("//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list']/div" + "/a")).size() - 4;
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list']/div/a")));
+            int menuItems =
+                    driver.findElements(By.xpath("//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list" +
+                            "']/div" + "/a")).size() - 4;
             Random random = new Random();
             int randomItem = random.nextInt(menuItems) + 1;
             Actions actions = new Actions(driver);
-            actions.moveToElement(driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav-list" + "']/div/a)[" + randomItem + "]"))).perform();
-            int menuItemsInner = driver.findElements(By.xpath("//div[@id='gmenu-tab-327']//div[@class='header__nav-list" + "-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category-col" + "']//a")).size();
+
+            actions.moveToElement(driver.findElement(By.xpath("(//div[@id = 'gmenu-tab-327']/nav[@class='header__nav" + "-list" + "']/div/a)[" + randomItem + "]"))).perform();
+            //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='header__drop header__drop--clothing']"))); //!
+            int menuItemsInner =
+                    driver.findElements(By.xpath("//div[@id='gmenu-tab-327']//div[@class='header__nav" + "-list" +
+                            "-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop" + "-category-col" + "']//a")).size();
             int randomInner = random.nextInt(menuItemsInner) + 1;
-            String randomItemInner = driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class='header__nav" + "-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category" + "-col']//a)[" + randomInner + "]")).getText().toLowerCase();
+            String randomItemInner = driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class" +
+                    "='header__nav" + "-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class" + "='header__drop-category" + "-col']//a)[" + randomInner + "]")).getText().toLowerCase();
             driver.findElement(By.xpath("(//div[@id='gmenu-tab-327']//div[@class='header__nav-list-item'][" + randomItem + "]//div[@class='header__drop-inner']/div[@class='header__drop-category-col']//a)[" + randomInner + "]")).click();
             String randomPageHeader = driver.findElement(By.tagName("h1")).getText().toLowerCase();
             Assert.assertTrue(randomPageHeader.contains(randomItemInner));
 
-            int catalogItems = driver.findElements(By.xpath("//a[@class='catalog__item catalog__item--new " + "owox-catalog-list']")).size();
-            System.out.println("Длина меню: " + catalogItems);
+            driver.findElement(By.xpath("//body")).sendKeys(Keys.PAGE_DOWN);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='catalog__list']")));
+            int catalogItems =
+                    driver.findElements(By.xpath("//a[@class='catalog__item catalog__item--new " + "owox" + "-catalog" +
+                            "-list']")).size();
             int randomCatalogItem = random.nextInt(catalogItems) + 1;
             driver.findElement(By.xpath("(//a[@class='catalog__item catalog__item--new owox-catalog-list'])[" + randomCatalogItem + "]")).click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card']")));
-            cartItemInfo.add(driver.findElement(By.xpath("//div[@class='card__info-title']")).getText().toLowerCase());
-            String nameOfProduct = driver.findElement(By.xpath("//div[@class='card__info-title']")).getText().toLowerCase();
+
+            String[] nameOfProduct =
+                    driver.findElement(By.xpath("//div[@class='card__info-title']")).getText().toLowerCase().replaceAll(" ","").split("\n");
+            cartItemName.add(nameOfProduct[0]);
+
+            System.out.println(nameOfProduct[0]);//!
+
             driver.findElement(By.xpath("//input[@class='js-select-input select__input']")).click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='select__drop select__drop--sizes']")));
-            cartItemInfo.add(driver.findElement(By.xpath("//ul[@id='offer-sizes']/li[contains(@class, 'selected')]")).getAttribute("data-size").toLowerCase());
-            String sizeOfProduct = driver.findElement(By.xpath("//ul[@id='offer-sizes']/li[contains(@class, 'selected')]")).getAttribute("data-size").toLowerCase();
-            cartItemInfo.add(driver.findElement(By.xpath("//div[@id='offer-price']/div")).getText().replaceAll(" ", ""));
-            String priceOfProduct = driver.findElement(By.xpath("//div[@id='offer-price']/div")).getText().replaceAll(" ", "");
-            driver.findElement(By.xpath("//div[@id='offer-price']/div")).click();
-            System.out.println("цена: " + priceOfProduct);
-            System.out.println("название: " + nameOfProduct);
-            System.out.println("размер: " + sizeOfProduct);
+            cartItemColor.add(driver.findElement(By.xpath("//div[@class='card__color-value']")).getText().toLowerCase());
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='select__drop " +
+                    "select__drop--sizes']")));
+            cartItemSize.add(driver.findElement(By.xpath("//ul[@id='offer-sizes']/li[contains(@class, 'selected')]")).getAttribute("data-size").toLowerCase());
+            //String sizeOfProduct = driver.findElement(By.xpath("//ul[@id='offer-sizes']/li[contains(@class,'selected')]")).getAttribute("data-size").toLowerCase();
+            cartItemPrice.add(driver.findElement(By.xpath("//div[@id='offer-price']/div")).getText().replaceAll(" ",
+                    "").replaceAll("₽", ""));
+            //String priceOfProduct = driver.findElement(By.xpath("//div[@id='offer-price']/div")).getText().replaceAll(" ", "").replaceAll("₽", "");
+            //driver.findElement(By.xpath("//div[@id='offer-price']/div")).click();
             driver.findElement(By.id("btn-add-to-cart")).click();
             driver.findElement(By.xpath("//a[@class='header__logo-link']")).click();
         }
-
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='basket-counter__value' and contains (text(),'2')]")));
+        System.out.println(cartItemName);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='basket-counter__value' and " + "contains (text(),'2')]")));
         driver.findElement(By.xpath("//a[@class='header__r-icons-link js-popup js-header-basket']")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='basket__non-empty']")));
-        for (int i = 0; i < 2; i++) {
+        Thread.sleep(2000);
+        int totalPrice = Integer.parseInt(driver.findElement(By.id("basket-total-price")).getText().replaceAll(" ",
+                ""));
 
-
-            inCartItemInfo.add(driver.findElement(By.xpath("//div[@class='basket__item-price']/div")).getText().replaceAll(" ", ""));
-            String priceOfProductInCart = driver.findElement(By.xpath("//div[@class='basket__item-price']/div")).getText().replaceAll(" ", "");
-            inCartItemInfo.add(driver.findElement(By.xpath("//div[@class='basket__item-title']")).getText().toLowerCase());
-            String nameOfProductInCart = driver.findElement(By.xpath("//div[@class='basket__item-title']")).getText().toLowerCase();
-            inCartItemInfo.add(driver.findElement(By.xpath("//div[@class='basket__item-prop']")).getText().replaceAll(" ", "").toLowerCase());
-            String sizeOfProductInCart = driver.findElement(By.xpath("//div[@class='basket__item-prop']")).getText().replaceAll(" ", "").toLowerCase();
-            System.out.println("цена в корзине: " + priceOfProductInCart);
-            System.out.println("имя в корзине: " + nameOfProductInCart);
-            System.out.println("размер в корзине: " + sizeOfProductInCart);
-        }
-        System.out.println("список: " + cartItemInfo);
-        System.out.println("список в корзине: " + inCartItemInfo);
-        /*Assert.assertTrue(priceOfProduct.contains(priceOfProductInCart));
-        Assert.assertTrue(nameOfProduct.contains(nameOfProductInCart));
-        Assert.assertTrue(sizeOfProductInCart.contains(sizeOfProduct));*/
         for (int i = 0; i < 2; i++) {
+            String priceOfProductInCart =
+                    driver.findElement(By.xpath("//div[@class='basket__item-price']/div")).getText().replaceAll(" ",
+                            "").replaceAll("₽", "");
+            Assert.assertTrue(cartItemPrice.contains(priceOfProductInCart));
+            String nameOfProductInCart =
+                    driver.findElement(By.xpath("//div[@class='basket__item-title']")).getText().toLowerCase().replaceAll("[^\\da-zA-Z]", "");
+            System.out.println(nameOfProductInCart);
+            Assert.assertTrue(cartItemName.contains(nameOfProductInCart));
+            String colorOfProductInCart= driver.findElement(By.xpath("//div[@class='basket__item-props']/div[(contains(text(), 'Цвет:'))]")).getText().toLowerCase().replaceAll("цвет:","").replaceAll(" ","");
+            Assert.assertTrue(cartItemColor.contains(colorOfProductInCart));
+            String sizeOfProductInCart =
+                    driver.findElement(By.xpath("//div[@class='basket__item-prop']")).getText().replaceAll(" ", "").toLowerCase().replaceAll("размер:", "");
+            Assert.assertTrue(cartItemSize.contains(sizeOfProductInCart));
             driver.findElement(By.xpath("//div[@class='basket__item-remove js-basket-remove-item']")).click();
         }
-        driver.findElement(By.xpath("//div[@class='basket__header-icon-box js-popup-close']")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'basket__empty-title')]")));
-        Thread.sleep(3000);
 
+        int sumInCarts = 0;
+
+        for (int i = 0; i<cartItemPrice.size(); i++){
+            sumInCarts += Integer.parseInt(cartItemPrice.get(i));
+        }
+        System.out.println(sumInCarts);
+        System.out.println(totalPrice);
+        Assert.assertEquals(sumInCarts,totalPrice);
+
+
+        driver.findElement(By.xpath("//div[@class='basket__header-icon-box js-popup-close']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, " + "'basket__empty" +
+                "-title')]")));
 
     }
 }
