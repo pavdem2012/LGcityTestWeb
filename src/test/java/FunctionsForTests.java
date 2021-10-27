89f31feba830beaa0d61c75601044c800c983972
pavdem2012 <pavdem2012@gmail.com>
2021-10-27 18:44:41 +0700

    *

--- src/test/java/FunctionsForTests.java
+++ src/test/java/FunctionsForTests.java
@@ -1,9 +1,5 @@
 import common.Settings;
-import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
-import pages.Pages;
 
-import java.util.Random;
 
 public class FunctionsForTests extends Settings {
 
