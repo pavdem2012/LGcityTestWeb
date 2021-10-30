56786a8b130742f2b46759a3b37dcb31ef15d346
Алексей <hell1k@yandex.ru>
2021-10-30 18:59:50 +0700

    favoritePage

--- src/test/java/common/Settings.java
+++ src/test/java/common/Settings.java
@@ -3,16 +3,20 @@ package common;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.BasketPage;
 import pages.CityPage;
+import pages.FavoritePage;
 import pages.MainPage;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Settings {
@@ -22,6 +26,8 @@ public class Settings {
     public CityPage cityPage;
     public BasketPage basketPage;
     public Pages pages;
+    public FavoritePage favoritePage;
+    public FunctionsForTests functions;
 
     @Before
     public void before() {
@@ -31,13 +37,16 @@ public class Settings {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         cityPage = new CityPage(driver, wait);
-        basketPage= new BasketPage(driver, wait);
+        basketPage = new BasketPage(driver, wait);
         pages = new Pages(driver, wait);
+        favoritePage = new FavoritePage(driver, wait);
+        functions = new FunctionsForTests();
     }
 
     public void open(String baseUrl) {
         driver.get(baseUrl);
         pages.closeCookieBtn.click();//!!!Не забыть перетащить элемент в MainPage
+        waitInvisibilityElement(pages.closeCookieBtn);
     }
 
     public WebElement getElementByXpath(String string) {
@@ -76,10 +85,20 @@ public class Settings {
         wait.until(ExpectedConditions.textToBePresentInElementValue(element, string));
     }
 
-    /*    public WebElement getElementsByXpath(String string) {
-            ArrayList<WebElement> list = driver.findElements(By.xpath(string));
-            return driver.findElements(By.xpath(string));
-        }*/
+    public void sendKeysToBody(Keys keys) {
+        driver.findElement(By.xpath("//body")).sendKeys(keys);
+    }
+
+    public int getRandom(int number) {
+        Random random = new Random();
+        return random.nextInt(number);
+    }
+
+    public void moveTo(WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+
     @After
     public void quit() {
         driver.quit();
--- src/test/java/tests/TestFavorites.java
+++ src/test/java/tests/TestFavorites.java
@@ -1,4 +1,61 @@
 package tests;
 
-public class TestFavorites {
+import common.Settings;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+public class TestFavorites extends Settings {
+
+    /*
+    Проверка добавления в "Избранное" из карточки товара
+     */
+    @Test
+    public void addFavoritesFromCartOfProduct() {
+        open("https://lgcity.ru");
+        waitVisibilityElement(favoritePage.menuItem);
+        favoritePage.selectRandomMenu();
+        String randomMenuItem = favoritePage.selectRandomMenuItem();
+        Assert.assertTrue(favoritePage.getTitle().contains(randomMenuItem));
+        sendKeysToBody(Keys.PAGE_DOWN);
+        waitVisibilityElement(favoritePage.catalogListBlock);
+        favoritePage.selectRandomCard();
+        waitVisibilityElement(favoritePage.cardPage);
+        String cardTitle = favoritePage.getCardTitle();
+        favoritePage.addToFavoriteBtn.click();
+//        favoritePage.addToFavoriteIcon.isEnabled();
+        waitVisibilityElement(favoritePage.favoriteCounter1);
+        favoritePage.favoriteInHeader.click();
+        waitVisibilityElement(favoritePage.favoriteBlock);
+        Assert.assertTrue(cardTitle.contains(favoritePage.getFavoriteCardTitle()));
+        favoritePage.deleteFavoriteBtn.click();
+        waitVisibilityElement(favoritePage.favoriteEmptyPage);
+        favoritePage.goToMainPage.click();
+        waitVisibilityElement(favoritePage.favoriteCounter0);
+    }
+
+    /*
+    Проверка добавления в "Избранное" из каталога
+     */
+    @Test
+    public void addFavoritesFromCatalogue() {
+        open("https://lgcity.ru");
+        waitVisibilityElement(favoritePage.menuItem);
+        favoritePage.selectRandomMenu();
+        String randomMenuItem = favoritePage.selectRandomMenuItem();
+        Assert.assertTrue(favoritePage.getTitle().contains(randomMenuItem));
+        sendKeysToBody(Keys.PAGE_DOWN);
+        waitVisibilityElement(favoritePage.catalogListBlock);
+        String itemTitleInCatalog = favoritePage.selectFavoritesIcons();
+//        favoritePage.addToFavoriteIcon.isEnabled();
+        moveTo(favoritePage.headerElement);
+        waitVisibilityElement(favoritePage.favoriteCounter1);
+        favoritePage.favoriteInHeader.click();
+        waitVisibilityElement(favoritePage.favoriteBlock);
+        Assert.assertTrue(itemTitleInCatalog.contains(favoritePage.getFavoriteCardTitle()));
+        favoritePage.deleteFavoriteBtn.click();
+        waitVisibilityElement(favoritePage.favoriteEmptyPage);
+        favoritePage.goToMainPage.click();
+        waitVisibilityElement(favoritePage.favoriteCounter0);
+    }
 }
