1c4bb582bfceaee1981f9b3440db3f678e47ee07
pavdem2012 <pavdem2012@gmail.com>
2021-11-17 17:51:38 +0700

    add some active comment in code

--- src/test/java/tests/TestMainMenu.java
+++ src/test/java/tests/TestMainMenu.java
@@ -1,6 +1,9 @@
 package tests;
 
 import common.Settings;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 
 public class TestMainMenu extends Settings {
@@ -8,7 +11,10 @@ public class TestMainMenu extends Settings {
     /*
     Проверка элементов главного меню.
      */
-    @Test
+    @Epic(value = "Главная страница")
+    @Feature(value = "Главное меню")
+    @Description("Проверка перехода из главного меню в каталог")
+    @Test(description="Проверка перехода из главного меню в каталог")
     public void testMenu() throws InterruptedException {
         open("https://lgcity.ru");
         pages.menuItem.isEnabled();
--- src/test/java/tests/testMainPage.java
+++ src/test/java/tests/testMainPage.java
@@ -11,10 +11,7 @@ public class testMainPage extends Settings {
     /*
 Проверка элементов главного меню.
  */
-    @Epic(value = "Главная страница")
-    @Feature(value = "Главное меню")
-    @Description("Проверка перехода из главного меню в каталог")
-    @Test(description="Проверка перехода из главного меню в каталог")
+
     public void testMenu() throws InterruptedException {
         open("https://lgcity.ru");
         waitVisibilityElement(favoritePage.menuItem,"элемент подкатегории товаров");
