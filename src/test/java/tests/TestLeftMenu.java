6f6aca71e857e1daed5111b7d8e0e831fdf117ba
pavdem2012 <pavdem2012@gmail.com>
2021-12-08 18:09:06 +0700

    add annotations for testLeftMenu method

--- src/test/java/tests/TestLeftMenu.java
+++ src/test/java/tests/TestLeftMenu.java
@@ -3,11 +3,17 @@ package tests;
 import common.Settings;
 import common.TestData;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 
 
-@Test
 public class TestLeftMenu extends Settings {
+    @Epic(value = "Левое меню")
+    @Feature(value = "Проверка левого меню по категориям и подкатегориям")
+    @Description("Проверка левого меню по категориям и подкатегориям")
+    @Test
     public void testLeftMenu() throws InterruptedException {
         openWithCloseCookie(TestData.getProperty("baseUrl"));
         waitVisibilityElement(favoritePage.menuItem,"элемент подкатегории товаров");
